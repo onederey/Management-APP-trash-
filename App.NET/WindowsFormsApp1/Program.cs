52b46e5cbfc6b11166897e6fc2b054914871164a
onederey <morozovcustom@yandex.ru>
2021-10-13 19:43:08 +0400

    init2

---
+++ App.NET/WindowsFormsApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace WindowsFormsApp1
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		[Obsolete]
+		static void Main()
+		{
+			SqlConnection conn = new SqlConnection();
+			conn.ConnectionString = ConfigurationManager.ConnectionStrings["connString"].ConnectionString;
+			
+
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Form1());
+		}
+	}
+}
